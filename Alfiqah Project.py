4f2c825fe294ad703ddc4d28b8831cf72f8b8381
alfiqah <85738046+alfiqah@users.noreply.github.com>
2021-06-11 17:02:54 +0700

    Add files via upload

---
+++ Alfiqah Project.py
@@ -0,0 +1,19 @@
+print("__________________________________________________")
+print("---------------------------Selamat Datang------------")
+print("__________________________________________________")
+apel = int (input("|Masukan Banyak Apel :"))
+jeruk = int(input("|Masukan Banyak Jeruk:"))
+
+hapel=6000
+hjeruk=8000
+total=(hapel*apel+hjeruk*jeruk)
+
+print("|Harga Apel = Rp.6000 X",apel,"buah")
+print("|Harga Jeruk = Rp.8000 X",jeruk,"buah")
+print("|Total Bayar = Rp.",total)
+bayar = int (input("|Uang Bayar = Rp. "))
+print("|Kembalian = Rp.", bayar-total)
+print("_____________________________________________")
+print("-----------------Terima Kasih------------------")
+
+print("____________________________________________")
